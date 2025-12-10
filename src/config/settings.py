acbcdcda6d8ffd3db95f1f67a4f841da75ad5aaf
TheoFBS <theo.fonseca.br.silva@gmail.com>
2025-12-10 11:32:18 -0300

    Add files via upload

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,5 +2,10 @@ BOT_CREDS = "src/keys/bot_creds.json"
 CREDS = "src/keys/credentials.json"
 TOKEN = "src/keys/token.json"
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
 OUTPUT_FOLDER = "src/output/"
 TEMPLATE_FILE = "src/templates/Guia de Aprendizagem.docx"
+
+ID_FUNDAMENTAL_I = "1LSEerPaaokPQHR3Ov0miPCQqUGUyumobVUWIv49y4No"
+ID_FUNDAMENTAL_II = "1LoQ3ZMVQRlJnqG6y_z9QTlfKIPsZsTPzNjNA3Ht_j7w"
+ID_MEDIO = "1iGmcLIVsrxEBpMiK0q8BZcSA-6Y_u5fNW0_Tck_AdQ8"
